614a8d44e8f922c953bd52098d5c2a6b95cb5c2e
Aaron Lewis <amlewis@amlewis-ThinkPad-T400.(none)>
2012-12-04 12:29:54 -0800

    Setting up USB HID

---
+++ firmware/src/saxtutor.c
@@ -0,0 +1,84 @@
+/* LED Blink Example with USB Debug Channel for Teensy USB Development Board
+ * http://www.pjrc.com/teensy/
+ * Copyright (c) 2008, 2010 PJRC.COM, LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <stdlib.h>
+#include <string.h>
+#include <util/delay.h>
+#include "usb_rawhid.h"
+
+// Teensy 2.0: LED is active high
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_AT90USB1286__)
+#define LED_ON		(PORTD |= (1<<6))
+#define LED_OFF		(PORTD &= ~(1<<6))
+
+// Teensy 1.0: LED is active low
+#else
+#define LED_ON	(PORTD &= ~(1<<6))
+#define LED_OFF	(PORTD |= (1<<6))
+#endif
+
+#define LED_CONFIG	(DDRD |= (1<<6))
+#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
+
+int main(void)
+{
+	unsigned char i;
+	int8_t r;
+	int32_t x;
+	uint8_t* buffer = (uint8_t*)malloc(sizeof(int));
+
+	// set for 16 MHz clock, and make sure the LED is off
+	CPU_PRESCALE(0);
+	LED_CONFIG;
+	LED_ON;
+	
+	// Configure inputs to be pull up
+	DDRC &= ~(1<<0);
+	PORTC |= (1<<0);
+
+	usb_init();
+
+	//Wait for usb to be configured
+	while (!usb_configured());
+
+	// blink morse code messages!
+	while (1) {
+		if (PINC & (1<<0)) {
+			LED_ON;
+			x = 0;
+			memcpy(buffer, &x, sizeof(int));
+		} else {
+			LED_OFF;
+			x = -1;
+			memcpy(buffer, &x, sizeof(int));
+		}
+
+		r = usb_rawhid_recv(buffer, 0);
+		if (r > 0) { //Send a packet back
+			usb_rawhid_send(buffer, 0);
+		}
+		_delay_ms(5);
+	}
+}
